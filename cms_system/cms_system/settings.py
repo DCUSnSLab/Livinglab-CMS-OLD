981dcf568085ce1ce1722c942a21fec8d4cabc69
Kim-Bo-Kyeong <ym1021@cu.ac.kr>
2022-02-14 15:20:06 +0900

    community20220214

--- cms_system/cms_system/settings.py
+++ cms_system/cms_system/settings.py
@@ -99,10 +99,10 @@ DATABASES = {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
-        'NAME': 'livinglab_04',
-        'PASSWORD': '0110',
+        'NAME': 'livinglab01',
+        'PASSWORD': '011021',
         'PORT': '3306',
-        'USER': 'master04'
+        'USER': 'master'
     }
 }
 
