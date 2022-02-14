39aef41adc33d30ce7025a877af4b302b5959673
Kim-Bo-Kyeong <ym1021@cu.ac.kr>
2022-02-14 15:56:33 +0900

    community20220214_1556

--- cms_system/community/urls.py
+++ cms_system/community/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from community.views import index, community, posting, new_post, remove_post, reply, edit_post, edit_reply, remove_reply
+from community.views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('community/<int:id>/', community, name='community'),
-    path('community/<int:id>/<int:pk>', posting, name="posting"),
-    path('community/<int:id>/new_post/', new_post, name='new_post'),
-    path('community/<int:id>/<int:pk>/edit', edit_post, name='edit_post'),
-    path('community/<int:id>/<int:pk>/remove/', remove_post, name='remove_post'),
-    path('community/<int:id>/<int:pk>/reply/', reply, name='reply'),
-    path('community/<int:id>/<int:pk>/reply/<int:rid>/edit', edit_reply, name='edit_reply'),
-    path('community/<int:id>/<int:pk>/reply/<int:rid>/remove', remove_reply, name='remove_reply'),
+    path('<int:id>/', community, name='community'),
+    path('<int:id>/<int:pk>', posting, name="posting"),
+    path('<int:id>/new_post/', new_post, name='new_post'),
+    path('<int:id>/<int:pk>/edit', edit_post, name='edit_post'),
+    path('<int:id>/<int:pk>/remove/', remove_post, name='remove_post'),
+    path('<int:id>/<int:pk>/reply/', reply, name='reply'),
+    path('<int:id>/<int:pk>/reply/<int:rid>/edit', edit_reply, name='edit_reply'),
+    path('<int:id>/<int:pk>/reply/<int:rid>/remove', remove_reply, name='remove_reply'),
+    path('<int:id>/<int:pk>/rereply/<int:rid>', rereply, name='rereply'),
 ]
